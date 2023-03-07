33be9112c78c1516a6c3313cc42b9ca27fd2ff4b
ABiviano13 <abiviano0@outlook.com>
2023-03-07 18:24:40 +0100

    FEAT main card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,27 @@
 <script>
+import axios from 'axios'
 
 export default {
+
+    data() {
+        return {
+            filmArray: []
+        }
+    },
+    methods: {
+        fechFilm() {
+            axios
+                .get('https://api.themoviedb.org/3/search/movie?api_key=9feb0690aacb6d5cd46a246f69bb9918&query=ironman')
+                .then((response) => {
+                    console.log(response)
+                    this.filmArray = response.data.results
+                    console.log(this.filmArray)
+                })
+        }
+    },
+    created() {
+        this.fechFilm()
+    }
 
   
 }
@@ -11,8 +32,18 @@ export default {
     <main class="main-content">
         <div class="container">
             <ul class="list-card">
-                <li class="card"> 
-                    ciao
+                <li class="card"
+                v-for="cardFilm in filmArray"
+                > 
+                    <h2 class="title-film">
+                        {{ cardFilm.title }}
+                    </h2>
+                    <h3 class="title-original-film">
+                        {{ cardFilm.original_title }}
+                    </h3>
+                    <div class="description-film">
+                        {{ cardFilm.overview }}
+                    </div>
                 </li>
             </ul>
         </div>
@@ -25,6 +56,12 @@ export default {
 .main-content{
     background-color: gray;
     height: 100vh;
+
+    .list-card{
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+        gap: 15px;
+    }
 }
 
 </style>
